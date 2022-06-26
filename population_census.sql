52762114d1701e482a371803a480475d623d40b3
Stephane Popov <73848169+kiwidreamz@users.noreply.github.com>
2022-06-26 18:59:25 +0200

    population_census

---
+++ population_census.sql
@@ -0,0 +1,3 @@
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+select sum(population) from city where countrycode = ANY (select code from country where continent = "Asia");
